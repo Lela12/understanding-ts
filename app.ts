4ef0f45ae6167f3214d4d23f112768f4d5686a2b
Lela12 <dbcocotn4@hanmail.net>
2022-01-10 23:36:17 +0900

    Using Types

---
+++ app.ts
@@ -0,0 +1,10 @@
+function add(n1: number, n2: number) {
+  //유형을 할당하고자 하는 값 뒤에 콜론 추가
+  return n1 + n2;
+}
+
+const number1 = "5";
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
